74a858bbc9a92e761cfa5259498ce4226f5970b9
Krystian <krystian.szmigielski2@gmail.com>
2025-02-07 20:04:40 +0100

    Add swagger

--- SocialMedia.API/Program.cs
+++ SocialMedia.API/Program.cs
@@ -7,11 +7,30 @@ builder.AddServiceDefaults();
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 DiConfig.ConfigureServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment()) app.MapOpenApi();
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUi(options =>
+    {
+        options.DocumentPath = "/openapi/v1.json";
+    });
+}
+
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
