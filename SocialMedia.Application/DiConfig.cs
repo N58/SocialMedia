aa372e72afb69db0c8722184f5939e9c49f4cd53
Krystian <krystian.szmigielski2@gmail.com>
2025-02-07 20:06:39 +0100

    Refactor

--- SocialMedia.Application/DiConfig.cs
+++ SocialMedia.Application/DiConfig.cs
@@ -1,7 +1,5 @@
 using FluentValidation;
 using FluentValidation.AspNetCore;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SocialMedia.Application.Commands.CreatePost;
