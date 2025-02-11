c19bb0bd44e4fcf4a4c984f6fbdf683d7dce1940
Krystian <krystian.szmigielski2@gmail.com>
2025-02-11 18:57:42 +0100

    Change convertion of date to DateTime instead LocalDateTime

--- SocialMedia.API/Converters/DateTimeTypeConverter.cs
+++ SocialMedia.API/Converters/DateTimeTypeConverter.cs
@@ -6,6 +6,6 @@ public class DateTimeTypeConverter : ITypeConverter<DateTimeOffset, DateTime>
 {
     public DateTime Convert(DateTimeOffset source, DateTime destination, ResolutionContext context)
     {
-        return source.LocalDateTime;
+        return source.DateTime;
     }
 }
